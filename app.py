6f99cca6a4553e3acdebab2512ce3d820fe69f76
kanupriya-sharma19 <sharmakanpuriya@gmail.com>
2025-05-04 21:58:56 +0530

    Sound generation

--- app.py
+++ app.py
@@ -1,59 +1,35 @@
-from flask import Flask, render_template,request,redirect
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, request
+import requests
+from flask_cors import CORS
+
 app = Flask(__name__)
-from datetime import datetime
-app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///todo.db"
-app.config['SQLALCHEMY_DATABASE_MODIFICATIONS']=False
+app = Flask(__name__)
+CORS(app) 
+API_KEY = "7vwLRNs7rNgUWFcpTtUA1Zmcwe0iV6jNwVCiv0wp"
+headers = {"Authorization": f"Token {API_KEY}"}
 
-db=SQLAlchemy(app)
+@app.route('/search_sound', methods=['GET'])
+def search_sound():
+    query = request.args.get('query', default='Water', type=str)
 
-class Todo(db.Model):
-    sno=db.Column(db.Integer,primary_key=True)
-    title=db.Column(db.String(200),nullable=False)
-    desc=db.Column(db.String(500),nullable=False)
-    date_created=db.Column(db.DateTime,default=datetime.utcnow)
+    search_url = "https://freesound.org/apiv2/search/text/"
+    params = {
+        "query": query,
+        "filter": "duration:[1.0 TO 5.0]",
+        "fields": "id,name,previews",
+        "page_size": 1
+    }
 
-    def __repr__(self)->str:
-        return f"{self.sno}-{self.title}"
-    #kya print hona chahiye when db runs
+    response = requests.get(search_url, headers=headers, params=params)
+    results = response.json()
+    print(response.text)
+    if results.get('results'):
+        sound = results['results'][0]
+        preview_url = sound['previews']['preview-hq-mp3']
+        return jsonify({'success': True, 'sound': preview_url})
+    else:
+        return jsonify({'success': False, 'message': 'No sound found'})
 
-@app.route("/",methods=['GET','POST'])
-
-def hello_world():
-    if request.method=='POST':
-        title=request.form['title']
-        desc=request.form['desc']
-        todo=Todo(title=title,desc=desc)
-        db.session.add(todo)
-        db.session.commit()
-    allTodo=Todo.query.all()
-   
-    # return "<p>Hello, World!</p>"
-    return render_template('index.html',allTodo=allTodo)
-
-@app.route('/delete/<int:sno>')
-def delete(sno):
-    todo=Todo.query.filter_by(sno=sno).first()
-    db.session.delete(todo)
-    db.session.commit()
-    return redirect('/')
-
-@app.route('/update/<int:sno>',methods=['GET','POST'])
-def update(sno):
-    if request.method=='POST':
-        title=request.form['title']
-        desc=request.form['desc']
-        todo=Todo.query.filter_by(sno=sno).first()
-        todo.title=title
-        todo.desc=desc
-        db.session.add(todo)
-        db.session.commit()
-        return redirect("/")
-    todo=Todo.query.filter_by(sno=sno).first()
-    return render_template('update.html',todo=todo)
-
-
-if __name__=="__main__":
-    app.run(debug=True,port=8000) #run the app in debug mode
-    #defalut port is 5000
-    #staic ko render karva sakte hai..
+if __name__ == '__main__':
+    app.run(debug=True)
+app.run(debug=True, host='0.0.0.0', port=5000)
